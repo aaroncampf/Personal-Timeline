82326ceab3a92ae6e8a18a241d786bee94489169
Aaron Campf <aaroncampf@gmail.com>
2016-08-08 12:33:13 -0700

    Added Stuff

---
+++ PersonalTimeline/PersonalTimeline/Database/Activity.vb
@@ -0,0 +1,6 @@
+﻿Public Class Activity
+    Public Property id As Integer
+    Public Property Name As Date
+    Public Property Description As Integer
+    Public Property Notes As Integer
+End Class
