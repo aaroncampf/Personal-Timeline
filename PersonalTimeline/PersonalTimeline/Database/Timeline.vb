afd68cc94d53cad748c762ceabe6dfbe4eea7ae1
Aaron Campf <aaroncampf@gmail.com>
2016-08-12 11:50:00 -0700

    Adding Code

---
+++ PersonalTimeline/PersonalTimeline/Database/Timeline.vb
@@ -0,0 +1,8 @@
+﻿Public Class Timeline
+    Public Property id As Integer
+    Public Property Time As Date
+    Public Property Comments As String
+    Public Property Flag As Boolean
+    Public Property MetaData_Primary As String
+    Public Property MetaData_Secondary As String
+End Class
